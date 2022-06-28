05d3ef373d69f9e97de6088289a46c4602983eec
Fabricio Oliveira <fabricio.v.o@hotmail.com>
2022-06-27 21:01:22 -0300

    Ajustes de estilo e texto

--- src/Components/TitleScreen/TitleScreen.js
+++ src/Components/TitleScreen/TitleScreen.js
@@ -2,14 +2,14 @@ import { useDispatch } from "react-redux";
 import { changeBackground } from "../../Redux/backgroundSlice";
 import CanvasImage from "../CanvasImage/CanvasImage";
 import { Link } from "react-router-dom";
-
+import "./style.css";
 export default function TitleScreen() {
   const dispatch = useDispatch();
 
   return (
     <div className={"container"}>
       <CanvasImage width={121} height={80} />
-      <div>
+      <div className="start-button">
         <Link
           to={`/Game/0`}
           onClick={() =>
